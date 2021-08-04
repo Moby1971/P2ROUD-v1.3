cca94be986ed895efb38855c3744455c28afcb0c
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2021-08-04 10:01:05 +0200

    Bug fixes

--- Export/export_dicom_DCM.m
+++ Export/export_dicom_DCM.m
@@ -1,10 +1,5 @@
-function export_dicom_DCM(app,dcmdir,directory,image,parameters,tag)
+function export_dicom_DCM(app,dcmdir,directory,image,parameters,~)
 
-
-% create folder if not exist, and delete folder content
-folder_name = [directory,[filesep,'DICOM-',tag]];
-if (~exist(folder_name, 'dir')); mkdir(folder_name); end
-delete([folder_name,filesep,'*']);
 
 % Phase orientation
 if isfield(app.seqpar, 'PHASE_ORIENTATION')
@@ -14,13 +9,15 @@ if isfield(app.seqpar, 'PHASE_ORIENTATION')
     end
 end
 
+
 % size of the data
 dimx = size(image,1);
 dimy = size(image,2);
 dimz = size(image,3);
-nr_frames = size(image,4);
+NR = size(image,4);
 NFA = size(image,5);
 NE = size(image,6);
+
 
 % reading in the DICOM header information
 listing = dir(fullfile(dcmdir, '*.dcm'));
@@ -28,14 +25,22 @@ dcmfilename = [listing(1).folder,filesep,listing(1).name];
 base_header = dicominfo(dcmfilename);
 TextMessage(app,strcat('Reading DICOM info from',{' '},dcmfilename));
 
-% export the dicom images
 
+% create folder if not exist, and delete folder content
+dir1 = base_header.PatientName.FamilyName;
+dir2 = base_header.PatientID;
+dir3 = strcat(num2str(base_header.SeriesNumber),'P');
+folder_name = strcat(directory,filesep,dir1,filesep,dir2,filesep,dir3);
+if (~exist(folder_name, 'dir')); mkdir(fullfile(directory, dir1, dir2, dir3)); end
+delete([folder_name,filesep,'*']);
+
+
+% export the dicom images
 filecounter = 0;
 app.ExportProgressGauge.Value = 0;
-totalnumberofimages = nr_frames*NFA*NE*dimz;                    
+totalnumberofimages = NR*NFA*NE*dimz;                    
 
-
-for i=1:nr_frames % loop over all repetitions
+for i=1:NR % loop over all repetitions
     
     for j=1:NFA      % loop over all flip angles
         
@@ -47,12 +52,12 @@ for i=1:nr_frames % loop over all repetitions
                 filecounter = filecounter + 1;
                 
                 % File name
-                fn = ['00000',num2str(filecounter)];
+                fn = ['000000',num2str(filecounter)];
                 fn = fn(size(fn,2)-5:size(fn,2));
-                fname = [folder_name,filesep,'DICOM-XD-',fn,'.dcm'];
+                fname = [folder_name,filesep,fn,'.dcm'];
                 
                 % Dicom header
-                dcm_header = generate_dicomheader_DCM(app,base_header,parameters,fname,filecounter,i,j,k,z,dimx,dimy,dimz,nr_frames);
+                dcm_header = generate_dicomheader_DCM(app,base_header,parameters,fname,filecounter,i,j,k,z,dimx,dimy,dimz,NR);
                 
                 % The image
                 im = rot90(squeeze(cast(round(image(:,:,z,i,j,k)),'uint16')));
--- Export/generate_dicomheader_DCM.m
+++ Export/generate_dicomheader_DCM.m
@@ -1,4 +1,4 @@
-function dicom_header = generate_dicomheader_DCM(app,dcmhead,parameters,fn,filecounter,frame,j,k,slice,dimx,dimy,dimz,nr_frames)
+function dicom_header = generate_dicomheader_DCM(app,dcmhead,parameters,fname,filecounter,dyncnt,FAcnt,TEcnt,slicecnt,dimx,dimy,dimz,nr_dynamics)
 
 %
 % GENERATES DICOM HEADER FOR EXPORT
@@ -13,7 +13,7 @@ function dicom_header = generate_dicomheader_DCM(app,dcmhead,parameters,fn,filec
 % 
 %
 
-frametime = parameters.acqdur/nr_frames;    % time between frames in ms
+frametime = 1000*parameters.acqdur/nr_dynamics;    % time between frames in ms
 
 if app.seqpar.PHASE_ORIENTATION == 1
     pixely = app.FOVViewField1.Value/dimy;
@@ -23,20 +23,6 @@ else
     pixelx = app.FOVViewField1.Value/dimx;
 end
 
-fn = ['0000',num2str(frame)];
-fn = fn(size(fn,2)-4:size(fn,2));
-fname = ['DICOM_',fn,'.dcm'];
-
-dt = datetime(parameters.date,'InputFormat','dd-MMM-yyyy HH:mm:ss');
-year = num2str(dt.Year);
-month = ['0',num2str(dt.Month)]; month = month(end-1:end);
-day = ['0',num2str(dt.Day)]; day = day(end-1:end);
-date = [year,month,day];
-
-hour = ['0',num2str(dt.Hour)]; hour = hour(end-1:end);
-minute = ['0',num2str(dt.Minute)]; minute = minute(end-1:end);
-seconds = ['0',num2str(dt.Second)]; seconds = seconds(end-1:end);
-time = [hour,minute,seconds];
 
 dcmhead.Filename = fname;
 dcmhead.FileModDate = parameters.date;
@@ -53,21 +39,19 @@ dcmhead.PhysicianReadingStudy.FamilyName = 'Amsterdam UMC preclinical MRI';
 dcmhead.OperatorName.FamilyName = 'manager';
 dcmhead.ManufacturerModelName = 'MRS7024';
 dcmhead.ReferencedFrameNumber = [];  
-%dcmhead.PatientName.FamilyName = 'Amsterdam UMC preclinical MRI';
-%dcmhead.OtherPatientName.FamilyName = 'Amsterdam UMC preclinical MRI';
 dcmhead.NumberOfAverages = parameters.NO_AVERAGES;
 dcmhead.InversionTime = 0;
 dcmhead.ImagedNucleus = '1H';
 dcmhead.MagneticFieldStrength = 7;
-dcmhead.TriggerTime = (frame-1)*frametime;    % frame time 
+dcmhead.TriggerTime = (dyncnt-1)*frametime;    % frame time (ms)
 dcmhead.AcquisitionMatrix = uint16([dimx 0 0 dimy])';
 dcmhead.AcquisitionDeviceProcessingDescription = '';
 dcmhead.AcquisitionDuration = parameters.acqdur;
 dcmhead.InstanceNumber = filecounter;          % instance number
-dcmhead.TemporalPositionIdentifier = frame;     % frame number
-dcmhead.NumberOfTemporalPositions = nr_frames;
-dcmhead.ImagesInAcquisition = nr_frames*dimz;
-dcmhead.TemporalPositionIndex = uint32([]);
+dcmhead.TemporalPositionIdentifier = dyncnt;     % frame number
+dcmhead.NumberOfTemporalPositions = nr_dynamics;
+dcmhead.ImagesInAcquisition = nr_dynamics*dimz;
+dcmhead.TemporalPositionIndex = dyncnt;
 dcmhead.Rows = dimy;
 dcmhead.Columns = dimx;
 dcmhead.PixelSpacing = [pixely pixelx]';
@@ -83,14 +67,14 @@ dcmhead.NumberOfSlices = dimz;
 
 
 dcmhead.SliceThickness = parameters.SLICE_THICKNESS;
-dcmhead.EchoTime = parameters.te*k;                 % ECHO TIME         
+dcmhead.EchoTime = parameters.te*TEcnt;                 % ECHO TIME         
 dcmhead.SpacingBetweenSlices = parameters.SLICE_SEPARATION/parameters.SLICE_INTERLEAVE;
 dcmhead.EchoTrainLength = parameters.NO_ECHOES;
-dcmhead.FlipAngle = parameters.flipanglearray(j);           % FLIP ANGLES
+dcmhead.FlipAngle = parameters.flipanglearray(FAcnt);           % FLIP ANGLES
 
 if isfield(dcmhead, 'SliceLocation')
     startslice = dcmhead.SliceLocation;
-    dcmhead.SliceLocation = startslice+(slice-1)*(parameters.SLICE_SEPARATION/parameters.SLICE_INTERLEAVE);
+    dcmhead.SliceLocation = startslice+(slicecnt-1)*(parameters.SLICE_SEPARATION/parameters.SLICE_INTERLEAVE);
 end
 
 dicom_header = dcmhead;
